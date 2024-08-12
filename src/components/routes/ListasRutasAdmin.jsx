3b530cce8fc23e74a77dd8b4699c02f3b1d30a6c
Fausto Fernandez <fausto44190084@gmail.com>
2024-08-11 22:25:52 -0300

    desarrolle listaRutasAdmin

---
+++ src/components/routes/ListasRutasAdmin.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import FormularioClase from "../clases/FormularioClase";
+import Administrador from "../pages/Administrador";
+
+const ListasRutasAdmin = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Administrador></Administrador>}
+      ></Route>
+      <Route
+        path="/crear"
+        element={<FormularioClase creando={true}></FormularioClase>}
+      ></Route>
+      <Route       
+        path="/editar/:id"
+        element={<FormularioClase creando={false}></FormularioClase>}
+      ></Route>
+    </Routes>
+  );
+};
+
+export default ListasRutasAdmin;
