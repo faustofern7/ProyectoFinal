6c8571d62f990b967e65cda804398b398390afaa
Fausto Fernandez <fausto44190084@gmail.com>
2024-08-11 22:21:10 -0300

    pagina de error 404

---
+++ src/components/pages/Error404.jsx
@@ -0,0 +1,14 @@
+import { Button, Container } from 'react-bootstrap';
+import error404 from "../../assets/error404.svg"
+import { Link } from 'react-router-dom';
+
+const Error404 = () => {
+    return (
+        <Container className='text-center py-2'>
+            <img src={error404} alt="" className='imgError404 mx-auto d-block' />
+            <Link to='/' className=" btn bgVerde border-0">Volver al inicio</Link>
+        </Container>
+    );
+};
+
+export default Error404;
